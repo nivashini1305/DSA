62c35a803fae3faa0c7ea2fd5cfde857f5c8742a
nivashini1305 <nivashinigovindaraj05@gmail.com>
2025-05-12 10:00:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Digits/count-digits.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.Scanner;
+
+class Main {
+
+    
+// } Driver Code Ends
+
+// User function Template for Java
+
+public static int countDigits(int n) {
+
+    // write your code here
+    
+    // return number of digits in n
+    String num=Integer.toString(n);
+    return num.length();
+}
+
+
+//{ Driver Code Starts.
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+        while (t-- > 0) {
+            int n = scn.nextInt();
+            System.out.println(countDigits(n));
+
+            System.out.println("~");
+        }
+        scn.close();
+    }
+}
+// } Driver Code Ends
