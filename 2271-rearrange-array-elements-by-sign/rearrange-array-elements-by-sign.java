28a2cfe2869dc313979c3028946e2d498118ae64
nivashini1305 <nivashinigovindaraj05@gmail.com>
2025-05-22 11:38:01 +0530

    Time: 3 ms (100.00%) | Memory: 85.6 MB (7.82%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,31 +1,49 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        ArrayList<Integer> pos= new ArrayList<>();
-        ArrayList<Integer> neg= new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]<0)
-            {
-                neg.add(nums[i]);
-            }
-            else
-            {
-                pos.add(nums[i]);
-            }
-        }
+        // BRUTE FORCE
+        // ArrayList<Integer> pos= new ArrayList<>();
+        // ArrayList<Integer> neg= new ArrayList<>();
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]<0)
+        //     {
+        //         neg.add(nums[i]);
+        //     }
+        //     else
+        //     {
+        //         pos.add(nums[i]);
+        //     }
+        // }
+        // int j=0;
+        // int k=0;
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     if(i%2==0)
+        //     {
+        //         nums[i]=pos.get(j);
+        //         j++;
+        //     }
+        //     else{
+        //        nums[i]=neg.get(k);
+        //        k++; 
+        //     }
+        // }
+        // return nums;
+        int arr[] = new int[nums.length];
         int j=0;
-        int k=0;
+        int k=1;
         for(int i=0;i<nums.length;i++)
         {
-            if(i%2==0)
+            if(nums[i]>0)
             {
-                nums[i]=pos.get(j);
-                j++;
+                arr[j]=nums[i];
+                j+=2;
             }
             else{
-               nums[i]=neg.get(k);
-               k++; 
+                arr[k]=nums[i];
+                k+=2;
             }
+
         }
-        return nums;
+        return arr;
     }
 }
