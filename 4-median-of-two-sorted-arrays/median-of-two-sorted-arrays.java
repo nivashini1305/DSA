58c15105cfcfa8097d06024246f0c2faa07da7ce
nivashini1305 <nivashinigovindaraj05@gmail.com>
2025-06-12 15:50:25 +0530

    Time: 1 ms (100.00%) | Memory: 46.1 MB (60.93%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,47 +1,49 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n2=(nums1.length+nums2.length)/2;
-        int n1=n2-1;
-        int ele1=-1;
-        int ele2=-1;
-        int count=0;
-        int i=0;
-        int j=0;
-        while(i<nums1.length && j<nums2.length)
+        if(nums1.length>nums2.length)
         {
-            if(nums1[i]<nums2[j])
+            return findMedianSortedArrays(nums2,nums1);
+        }
+        int n=(nums1.length+nums2.length);
+        int left=(n+1)/2;
+        int low=0;
+        int high=nums1.length;
+        while(low<=high)
+        {
+            int mid1=(low+high)/2;
+            int mid2=left-mid1;
+            int l1=Integer.MIN_VALUE;
+            int l2=Integer.MIN_VALUE;
+            int r1=Integer.MAX_VALUE;
+            int r2=Integer.MAX_VALUE;
+            if(mid1<nums1.length)
+                r1=nums1[mid1];
+            if(mid2<nums2.length)
+                r2=nums2[mid2];
+            if(mid1-1>=0)
+                l1=nums1[mid1-1];
+            if(mid2-1>=0)
+                l2=nums2[mid2-1];
+            if(l1<=r2 && l2<=r1)
             {
-                if(count==n1) ele1=nums1[i];
-                if(count==n2) ele2=nums1[i];
-                count++;
-                i++;
+                if((nums1.length+nums2.length)%2==1)
+                {
+                    return Math.max(l1,l2);
+                }
+                else
+                {
+                    return ((double)Math.max(l1,l2)+Math.min(r1,r2))/2.0;
+                }
+            }
+            else if(l1>r2)
+            {
+                high=mid1-1;
             }
             else
             {
-                if(count==n1) ele1=nums2[j];
-                if(count==n2) ele2=nums2[j];
-                count++;
-                j++;
+                low=mid1+1;
             }
         }
-        while(i<nums1.length)
-        {
-                if(count==n1  ) ele1=nums1[i];
-                if(count==n2) ele2=nums1[i];
-                count++;
-                i++;
-        }
-        while(j<nums2.length)
-        {
-                if(count==n1) ele1=nums2[j];
-                if(count==n2) ele2=nums2[j];
-                count++;
-                j++;
-        }
-        if((nums1.length+nums2.length)%2==1)
-        {
-            return (double)ele2;
-        }
-        return (double)((double)ele1 + (double)ele2) /2.0;
+        return 0;
     }
 }
