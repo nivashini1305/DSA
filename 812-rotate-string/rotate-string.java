0e01f79c88d5e65fadd7cd406b61cd99466e10dc
nivashini1305 <nivashinigovindaraj05@gmail.com>
2025-06-15 18:02:44 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (90.54%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -6,18 +6,10 @@ class Solution {
         }
         String ans=s+s;
         int ind=ans.indexOf(goal);
-        // System.out.println(ans+" "+ind);
         if(ind!=-1)
         {
             return true;
         }
-        // for(int i=0;i<goal.length();i++)
-        // {
-        //     if(goal.charAt(i)!=ans.charAt(ind))
-        //     {
-        //         return false;
-        //     }
-        // }
         return false;
     }
 }
