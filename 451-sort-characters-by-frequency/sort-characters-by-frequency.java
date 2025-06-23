66bab24c4573955518b110c062f8762ddc0486cb
nivashini1305 <nivashinigovindaraj05@gmail.com>
2025-06-23 12:51:10 +0530

    Time: 20 ms (18.44%) | Memory: 46 MB (21.44%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,37 +1,28 @@
 class Solution {
     public String frequencySort(String s) {
-        HashMap<Character,Integer> map = new HashMap<>();
+        HashMap<Character,Integer> map=new HashMap<>();
         for(int i=0;i<s.length();i++)
         {
-            char c = s.charAt(i);
-            if(map.containsKey(c))
-            {
-                map.put(c,map.get(c)+1);
-            }
-            else
-            {
-                map.put(c,1);
-            }
+            if(map.containsKey(s.charAt(i))) map.put(s.charAt(i),map.get(s.charAt(i))+1);
+            else map.put(s.charAt(i),1);
         }
         Comparator<Character> comp = new Comparator<>()
         {
-            public int compare(Character a1,Character a2)
+            public int compare(Character c1,Character c2)
             {
-                int diff = Integer.compare(map.get(a2),map.get(a1));
-                if(diff==0) return a1.compareTo(a2);
-                return diff;
+                int val=Integer.compare(map.get(c2),map.get(c1));
+                if(val==0) return c2.compareTo(c1);
+                return val;
             }
         };
-
         TreeMap<Character,Integer> tMap = new TreeMap<>(comp);
         tMap.putAll(map);
-
-        StringBuilder sb = new StringBuilder();
-        for(var a:tMap.keySet())
+        StringBuilder sb=new StringBuilder();
+        for(char c:tMap.keySet())
         {
-            for(int i=0;i<map.get(a);i++)
+            for(int i=0;i<map.get(c);i++)
             {
-                sb.append(a);
+                sb.append(c);
             }
         }
         return sb.toString();
