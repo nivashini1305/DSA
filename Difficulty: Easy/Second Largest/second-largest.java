49aedceeb1febd1de4b5bf8d3cec92de2555f150
nivashini1305 <nivashinigovindaraj05@gmail.com>
2025-07-23 19:25:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.java
+++ Difficulty: Easy/Second Largest/second-largest.java
@@ -1,50 +1,26 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-import java.util.stream.Collectors;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int t = Integer.parseInt(sc.nextLine());
-        while (t-- > 0) {
-            String[] arr1Str = sc.nextLine().split(" ");
-            int[] arr = Arrays.stream(arr1Str).mapToInt(Integer::parseInt).toArray();
-            Solution ob = new Solution();
-            int ans = ob.getSecondLargest(arr);
-            System.out.println(ans);
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
     public int getSecondLargest(int[] arr) {
         // code here
-        int fL=Integer.MIN_VALUE;
-        int sL=Integer.MIN_VALUE;;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==fL){
+        int max = Integer.MIN_VALUE;
+        int scndMax = Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]==max)
+            {
                 continue;
             }
-            if(arr[i]>fL){
-                sL=fL;
-                fL=arr[i];
+            if(arr[i] > max)
+            {
+                scndMax = max;
+                max=arr[i];
             }
-            else {
-                sL=Math.max(arr[i],sL);
+            else if(scndMax < arr[i])
+            {
+                scndMax=arr[i];
             }
         }
-        
-        if(sL==Integer.MIN_VALUE){
-            return -1;
-        }
-        return sL;
+        if(scndMax == Integer.MIN_VALUE) return -1;
+        return scndMax;
     }
 }
+
