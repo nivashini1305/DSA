5083145aab918428db8b7ce3db4418e5d208c2ac
nivashini1305 <nivashinigovindaraj05@gmail.com>
2025-08-10 11:45:07 +0530

    Time: 1 ms (22.07%) | Memory: 49 MB (62.19%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -1,12 +1,14 @@
 class Solution {
     public void reverseString(char[] s) {
+        int i=0;
         int p2 = s.length-1;
-        for(int i=0;i<s.length/2;i++)
+        while(i<p2)
         {
             char temp = s[i];
             s[i] = s[p2];
             s[p2] = temp;
             p2--;
+            i++;
         }
     }
 }
