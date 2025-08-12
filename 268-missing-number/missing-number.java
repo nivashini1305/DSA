c0a1ae2a38bc9a17a04dfdb2a7825162e1271bb9
nivashini1305 <nivashinigovindaraj05@gmail.com>
2025-08-12 12:46:02 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (93.90%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,13 +1,14 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int[] hashArray = new int[nums.length+1];
-        for(int singleNum:nums){
-            hashArray[singleNum]=singleNum;
-        }
-        for(int i=1;i<=nums.length;i++){
-            if(hashArray[i]==0){
-                return i;
-            }
+        int[] arr = new int[nums.length+1];
+        for(int i=0;i<nums.length;i++)
+        {
+            arr[nums[i]] = nums[i];
+        } 
+        for(int i=1;i<arr.length;i++)  
+        {
+            if(arr[i]==0)
+            return i;
         }
         return 0;
     }
