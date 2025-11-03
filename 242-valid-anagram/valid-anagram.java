8f5b8989e3de1be9640f246e2cb30834d1e13205
nivashini1305 <nivashinigovindaraj05@gmail.com>
2025-11-03 12:25:09 +0530

    Time: 7 ms (32.65%) | Memory: 46.5 MB (7.17%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        char[] chS = s.toCharArray();
+        char[] chT = t.toCharArray();
+        Arrays.sort(chS);
+        Arrays.sort(chT);
+        return Arrays.equals(chS,chT);
+    }
+}
