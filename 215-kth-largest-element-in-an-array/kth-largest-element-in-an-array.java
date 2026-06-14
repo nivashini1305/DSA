cd7e575acd198cda24c00ff16c21e2047120dbc1
nivashini1305 <nivashinigovindaraj05@gmail.com>
2026-06-14 12:32:30 +0530

    Time: 31 ms (78.58%) | Memory: 76 MB (5.17%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -3,10 +3,6 @@ class Solution {
         Arrays.sort(nums);
         int len = nums.length;
         int ans = nums[len-k];
-        // for(int i = len-1; i > len-k-1; i--)
-        // {
-        //     ans = nums[i]
-        // }
         return ans;
     }
 }
